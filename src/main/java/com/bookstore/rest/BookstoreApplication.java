b8cd28d4ad853ba5b592a37ed06dce90ab958665
yuchaudhari <42435306+yuchaudhari@users.noreply.github.com>
2018-08-20 12:42:18 +0530

    Update BookstoreApplication.java
    changes done

--- src/main/java/com/bookstore/rest/BookstoreApplication.java
+++ src/main/java/com/bookstore/rest/BookstoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.ashwani")
+@ComponentScan(basePackages = "com.bookstore")
 public class BookstoreApplication {
 
 	public static void main(final String[] args) {
